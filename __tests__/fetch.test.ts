38b8a3d1f0a1a7df2cad0308e98b44498bf0b95d
kjmin <leicht@kjmins-MacBook-Pro.local>
2020-09-26 21:37:50 +0900

    using builder in tests

--- __tests__/fetch.test.ts
+++ __tests__/fetch.test.ts
@@ -33,16 +33,20 @@ describe('craete instance', () => {
 
 describe('player', () => {
     it('search players by name', async () => {
-        const res = await fetch.players('steam', 'leichtjoon')
+        //const res = await fetch.players('steam', 'leichtjoon')
+        const res = await fetch.platform('steam').players('leichtjoon')
         expect(res).toHaveProperty('data')
         expect(Array.isArray(res.data)).toBe(true)
         expect(res).toMatchObject(testPlayers)
     })
     it('search player by identifier', async () => {
+        /*
         const res = await fetch.player(
             'steam',
             'account.183bc4b2c3404935baf3d56fb434b393',
         )
+        */
+        const res = await fetch.platform('steam').user('account.183bc4b2c3404935baf3d56fb434b393').json()
         expect(res).toHaveProperty('data')
         expect(res).toMatchObject(testPlayer)
     })
